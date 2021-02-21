8878d86555d50d8a7b4f327387b6699cadf35299
Jesse Gibson <JesseTheGibson@gmail.com>
2021-02-21 00:56:19 -0800

    Add device list diffing
    It automatically detects the difference between additions, deletions,
    and updates. Updates only happen when going from unpermissioned queries
    to permissioned queries.

--- src/__tests__/device-manager.test.ts
+++ src/__tests__/device-manager.test.ts
@@ -44,4 +44,65 @@ describe('DeviceManager', () => {
 
     expect(handler).toHaveBeenCalledTimes(1);
   });
+
+  it('detects removed devices', async () => {
+    setDeviceList([{}]);
+    const { devices } = setup();
+
+    const [device] = await devices.enumerate();
+    setDeviceList([]);
+    const handler = jest.fn();
+
+    devices.observe(handler);
+    await devices.enumerate();
+
+    expect(handler).toHaveBeenCalledWith([
+      { type: DeviceChangeType.Remove, device },
+    ]);
+  });
+
+  it('correlates identical devices between calls', async () => {
+    const [device] = setDeviceList([{ label: 'first' }]);
+    const { handler, devices } = setup();
+    await devices.enumerate();
+    setDeviceList([device, { label: 'second' }]);
+
+    handler.mockClear();
+    const [, secondDevice] = await devices.enumerate();
+
+    expect(handler).toHaveBeenCalledWith([
+      { type: DeviceChangeType.Add, device: secondDevice },
+    ]);
+  });
+
+  it('infers device relationships when the ID was just added', async () => {
+    const { handler, devices } = setup();
+    const kind = 'videoinput' as const;
+    const groupId = 'group-id';
+    const redactedDevice = { label: '', deviceId: '', groupId, kind };
+    const device = {
+      ...redactedDevice,
+      label: 'Creepy Shelf Elf',
+      deviceId: "sh'elf",
+    };
+
+    // Simulates fingerprinting countermeasures.
+    setDeviceList([redactedDevice]);
+    await devices.enumerate();
+
+    // Same device after the first approved `getUserMedia(...)` request.
+    setDeviceList([device]);
+
+    handler.mockClear();
+    await devices.enumerate();
+
+    // It should detect that it's the same device.
+    expect(handler).toHaveBeenCalledWith([
+      {
+        type: DeviceChangeType.Update,
+        oldInfo: { ...device, deviceId: null, label: null },
+        newInfo: device,
+      },
+    ]);
+  });
 });
--- src/device-manager.ts
+++ src/device-manager.ts
@@ -9,9 +9,7 @@ export default class DeviceManager {
   _knownDevices: Array<DeviceInfo> = [];
   _observers: Array<Callback> = [];
 
-  observe<Callback extends (changes: Array<DeviceChange>) => any>(
-    callback: Callback
-  ) {
+  observe<Fn extends Callback>(callback: Fn) {
     this._observers.push(callback);
   }
 
@@ -22,9 +20,14 @@ export default class DeviceManager {
     return devices;
   }
 
-  _checkForDeviceChanges(devices: Array<DeviceInfo>) {
-    const changes = this._calculateDeviceDiff(devices);
-    this._knownDevices = devices;
+  _checkForDeviceChanges(newDevices: Array<DeviceInfo>) {
+    const oldDevices = this._knownDevices;
+    this._knownDevices = newDevices; // Replace the old devices.
+
+    const changes: Array<DeviceChange> = this._calculateDeviceDiff(
+      newDevices,
+      oldDevices
+    );
 
     // Notify subscribers of any changes.
     if (changes.length) {
@@ -43,22 +46,97 @@ export default class DeviceManager {
    * correlate devices from permissioned requests with unpermissioned
    * requests.
    */
-  _calculateDeviceDiff(devices: Array<DeviceInfo>) {
-    // TODO: Implement other diff types.
-    if (this._knownDevices.length === devices.length) return [];
+  _calculateDeviceDiff(
+    newDevices: Array<DeviceInfo>,
+    oldDevices: Array<DeviceInfo>
+  ): Array<DeviceChange> {
+    const additions = newDevices.slice();
+    const removals = oldDevices.slice();
+    const updates: Array<DeviceChange> = [];
 
-    const changes = devices.map(device => ({
-      type: DeviceChangeType.Add,
-      device,
-    }));
+    // If a "new" device exists in the list of old devices, then it obviously
+    // wasn't just added and clearly we haven't removed it either. It's the
+    // same device.
+    additions.forEach((newDevice, newDeviceIndex) => {
+      const oldDeviceIndex = removals.findIndex(oldDevice => {
+        return isIdenticalDevice(newDevice, oldDevice);
+      });
 
-    return changes;
+      if (oldDeviceIndex > -1) {
+        additions.splice(newDeviceIndex, 1);
+        const [oldDevice] = removals.splice(oldDeviceIndex, 1);
+
+        if (!oldDevice.deviceId && newDevice.deviceId) {
+          const update: DeviceUpdateEvent = {
+            type: DeviceChangeType.Update,
+            newInfo: newDevice,
+            oldInfo: oldDevice,
+          };
+
+          updates.push(update);
+        }
+      }
+    });
+
+    return [
+      ...updates,
+
+      // A device was just removed.
+      ...removals.map(device => {
+        return { type: DeviceChangeType.Remove, device } as DeviceRemoveEvent;
+      }),
+
+      // A device was just plugged in.
+      ...additions.map(device => {
+        return { type: DeviceChangeType.Add, device } as DeviceAddEvent;
+      }),
+    ];
   }
 }
 
-interface DeviceChange {
-  type: DeviceChangeType;
+/**
+ * Due to fingerprinting countermeasures, the device ID might be an empty
+ * string. We have to resort to vague comparisons. After the first successful
+ * `getUserMedia(...)` query, the device ID for all related device kinds
+ * should be revealed. In that case the new device will have an ID but the old
+ * device won't. It should be safe to assume the inverse never happens.
+ *
+ * Note: Chromium browsers take a private stance by hiding your extra devices.
+ * Even if you have a hundred cameras plugged in, until that first GUM query,
+ * you'll only see the preferred one. Same for microphones and output devices.
+ */
+function isIdenticalDevice(newDevice: DeviceInfo, oldDevice: DeviceInfo) {
+  if (oldDevice.deviceId) {
+    return newDevice.deviceId === oldDevice.deviceId;
+  }
+
+  // These are the only credible fields we have to go on. It may yield a false
+  // positive if you're changing devices before the first GUM query, but since
+  // the lists are ordered by priority, that should be unlikely. It's
+  // certainly preferable to "new device" false positives.
+  function toCrudeId(device: DeviceInfo) {
+    return `${device.kind}:${device.groupId}`;
+  }
+
+  return toCrudeId(newDevice) === toCrudeId(oldDevice);
+}
+
+type DeviceChange = DeviceAddEvent | DeviceRemoveEvent | DeviceUpdateEvent;
+
+interface DeviceAddEvent {
+  type: DeviceChangeType.Add;
   device: DeviceInfo;
+}
+
+interface DeviceRemoveEvent {
+  type: DeviceChangeType.Remove;
+  device: DeviceInfo;
+}
+
+interface DeviceUpdateEvent {
+  type: DeviceChangeType.Update;
+  newInfo: DeviceInfo;
+  oldInfo: DeviceInfo;
 }
 
 export enum DeviceChangeType {
