e1ef99601353aad87ef1068eb393fb07f5b1454f
Jesse Gibson <JesseTheGibson@gmail.com>
2023-02-07 22:26:28 -0500

    Upgrade vite and remove dts
    I've found it's better to publish the `src` directory's index as the
    type definition than try to wrangle d.ts files. Maybe I will come to
    regret this, but it decouples me from the twice obsolete `vite-dts`
    plugin.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
 ## [UNRELEASED]
 
+### Changed
+
+- Reworked the build process to avoid an unmaintained plugin which affects how TypeScript definitions are published. There should not be any difference.
+
 ## [0.4.0] - 2022-05-30
 
 ### Removed
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite';
 import * as path from 'path';
-import dts from 'vite-dts';
 
 export default defineConfig({
-  plugins: [dts()],
   build: {
     lib: {
       entry: path.join(__dirname, './src/index.ts'),
       name: 'media-devices',
-      fileName: (format: string) => `media-devices.${format}.js`,
+      fileName: 'media-devices',
     },
     rollupOptions: {
       output: {
