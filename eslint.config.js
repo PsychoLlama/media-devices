ea7f620b00d931c1b91f7d5ccd17c0b3e0e7e1b2
Jesse Gibson <JesseTheGibson@gmail.com>
2025-08-03 11:47:17 -0400

    Update ESLint to v9
    There are new errors and warnings enabled in this version, or perhaps it
    was never configured correctly to begin with. There are a few true
    errors in the source code. I've ignored most of the errors in tests.

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+// @ts-check
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+        sourceType: 'module',
+      },
+    },
+  },
+  {
+    files: ['src/**/__tests__/**/*.test.ts'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/unbound-method': 'off',
+    },
+  }
+);
